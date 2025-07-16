3841abf7f9ddaf206c47389df595429eed30b5c6
Zafiadomgit <davidpietersjuegos@gmail.com>
2025-07-16 10:18:50 -0500

    chore: agregar configuración CSP para permitir Elfsight y Google Maps

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,28 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' https://static.elfsight.com 'unsafe-inline' 'unsafe-eval';
+              frame-src 'self' https://www.google.com https://static.elfsight.com;
+              child-src 'self' https://www.google.com https://static.elfsight.com;
+              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+              font-src 'self' https://fonts.gstatic.com;
+              img-src * blob: data:;
+              connect-src *;
+            `.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
+  },
 }
 
 export default nextConfig
